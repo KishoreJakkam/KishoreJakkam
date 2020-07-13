2098aeecbf75ca16e97ed1ca39a52e5287f9c2df
kishore <kishore@kishore-HP>
2020-07-13 22:32:19 +0530

    minor changes

--- GitDemo/src/TestHello.java
+++ GitDemo/src/TestHello.java
@@ -4,6 +4,7 @@ public class TestHello {
 	public static void main(String[] args) {
 		System.out.println("Hello,Welcome Git");
 		System.out.println("Hello,Dude How r u");
+		System.out.println("Updated on 13 July 1");
 	}
 
 }
