4b9f9bba55d99a9c044d07253201101e4f51c9b0
kishore <kishore@kishore-HP>
2020-07-26 19:24:45 +0530

    Azure Git comments

---
+++ MicrosoftAzure_GIT/src/test/java/MyTEST.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class MyTEST {
+	
+	
+	@Test()
+	public void test() throws Exception
+	{
+ 
+			
+		System.out.println("My TEST PROJECT1");
+	}
+
+
+}
